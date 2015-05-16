ccec2d521d60549ffd3019c007118c51f38cdf70
avmityakov <12>
2015-05-16 15:33:03 +0300

    set default port at first start

--- js/src/settings-page.js
+++ js/src/settings-page.js
@@ -3,14 +3,23 @@ var utils = require("./utils");
 var saveElement = document.querySelector("#save");
 var portElement = document.querySelector("#port");
 
+var DEFAULT_PORT = 8989;
+
 chrome.storage.sync.get( "port", function(data) {
   	if ( data && data["port"] ) {
   		portElement.value = data["port"];
   	}
+  	else {
+  		chrome.storage.sync.set({
+	    	port : DEFAULT_PORT
+	  	}, function() {
+	 		portElement.value = DEFAULT_PORT; 		
+	  	});
+  	}
 });
 
 saveElement.onclick = function() {
-	var port    = 8989;
+	var port    = DEFAULT_PORT;
 	var rawPort = portElement.value;
 
 	if ( utils.validatePort( rawPort ) ) {
